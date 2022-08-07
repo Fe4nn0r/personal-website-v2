8682e5713d4d31410bcfabed7cc0cc7d6742f3b5
Florent Clerc <florent.clerc@in-tact.fr>
2022-08-07 20:11:16 +0200

    add hamburger menu for mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,63 @@
 import type { NextPage } from "next";
+import { useState } from 'react';
 import { useTranslation } from "next-i18next";
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Head from "next/head";
-import Socials from '../components/Socials';
-import AboutMe from '../components/AboutMe';
+import { slide as Menu } from "react-burger-menu";
+import Socials from "../components/Socials";
+import AboutMe from "../components/AboutMe";
 import styles from "../styles/Home.module.css";
 import Experiences from "../components/Experiences";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
-import Footer from '../components/Footer';
+import Footer from "../components/Footer";
 
-export async function getStaticProps({ locale }: { locale: string}) {
+export async function getStaticProps({ locale }: { locale: string }) {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ['common', 'footer'])),
+      ...(await serverSideTranslations(locale, ["common", "footer"])),
       // Will be passed to the page component as props
     },
   };
 }
 
+function Nav({ className = "", closeMenu }: { className?: string; closeMenu: () => void }) {
+  const { t } = useTranslation();
+  return (
+    <div className={className}>
+      <nav>
+        <ul className={styles.mainNavList}>
+          <li>
+            <a className="animated-link" href="/#aboutme" onClick={closeMenu}>
+              {t("header.aboutMe")}
+            </a>
+          </li>
+          <li>
+            <a className="animated-link" href="/#skills" onClick={closeMenu}>
+              {t("header.skills")}
+            </a>
+          </li>
+          <li>
+            <a className="animated-link" href="/#experiences" onClick={closeMenu}>
+              {t("header.experiences")}
+            </a>
+          </li>
+          <li>
+            <a className="animated-link" href="/#projects" onClick={closeMenu}>
+              {t("header.projects")}
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <Socials />
+    </div>
+  );
+}
+
 const Home: NextPage = () => {
   const { t } = useTranslation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -29,28 +66,53 @@ const Home: NextPage = () => {
           name="description"
           content="Florent Clerc, 31 ans, développeur fullstack, lyonnais"
         />
-        <meta name="keywords" content="développeur frontend, développeur fullstack, cv développeur web, développeur web" />{" "}
-        <link rel="icon" href="/favicon.png" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="keywords"
+          content="développeur frontend, développeur fullstack, cv développeur web, développeur web"
+        />{" "}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon_io/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon_io/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon_io/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon_io/site.webmanifest" />{" "}
       </Head>
 
       <main>
         <header>
-          <nav>
-            <ul>
-              <li><a className="animated-link" href="/#aboutme">{t("header.aboutMe")}</a></li>
-              <li><a className="animated-link" href="/#skills">{t("header.skills")}</a></li>
-              <li><a className="animated-link" href="/#experiences">{t("header.experiences")}</a></li>
-              <li><a className="animated-link" href="/#projects">{t("header.projects")}</a></li>
-            </ul>
-          </nav>
-         <Socials />
+          <Menu
+            isOpen={isMenuOpen}
+            className={styles.mobileNav}
+            onStateChange={({ isOpen }: { isOpen: boolean }) => setIsMenuOpen(isOpen)}
+            right
+            customBurgerIcon={
+              <img src="/icons/menu-line-white.svg" height={60} alt={t('menu.openIcon')} />
+            }
+            customCrossIcon={
+              <img src="/icons/close-line-white.svg" height={60} alt={t('menu.openIcon')} />
+            }
+          >
+            <Nav closeMenu={() => setIsMenuOpen(false)} />
+          </Menu>
+
+          <Nav className={styles.desktopNav} closeMenu={() => setIsMenuOpen(false)} />
         </header>
         <AboutMe />
         <Skills />
         <Experiences />
         <Projects />
-
       </main>
 
       <Footer />
